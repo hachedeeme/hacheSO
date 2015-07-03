482f75cc672973e7b3627b961a59cba7b2a8cc23
unknown <publico@inforcytb02.alu.unq.edu.ar>
2015-07-02 21:17:14 -0800

    Round Robin added

--- kernelH/test/scheduling/testPolicy.py
+++ kernelH/test/scheduling/testPolicy.py
@@ -3,6 +3,7 @@ import queue
 
 from src.process.Pcb import Pcb
 from src.scheduling.policies.Fifo import Fifo
+from src.scheduling.policies.RoundRobin import RoundRobin
 
 class TestPolicy(unittest.TestCase):
     def setUp(self):
@@ -15,10 +16,22 @@ class TestPolicy(unittest.TestCase):
         ### Policies ###
         # First In First Out
         self.fifo = Fifo()
-        
+        self.rr   = RoundRobin(3)
+    
+    ###======================###
+    ### Test for FIFO policy ###    
+    ###======================###
     def test_choose_process_for_fifo(self):
         self.assertEquals(self.fifo.choose_pcb(self.queue).pid, 1)
         self.assertEquals(self.fifo.choose_pcb(self.queue).pid, 2)
+    
+    ###=============================###
+    ### Test for Round-Robin policy ###    
+    ###=============================###
+    def test_choose_process_for_round_robin(self):
+        self.assertEquals(self.rr.choose_pcb(self.queue).pid, 1)
+        self.assertEquals(self.rr.choose_pcb(self.queue).pid, 2)
+    
         
 if __name__ == '__main__':
     unittest.main()
